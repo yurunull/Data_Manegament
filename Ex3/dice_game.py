9715b59f498f759d332ba2c61b5f4cfd165a4dc9
yurunull <kamomeyuru@gmail.com>
2025-07-01 10:22:11 +0900

    Dice game: initial revision

---
+++ Ex3/dice_game.py
@@ -0,0 +1,11 @@
+import random
+
+print("Rolling dice...")
+
+die1 = random.randint(1, 6)
+die2 = random.randint(1, 6)
+total = die1+die2
+
+print(f"Die 1: {die1}")
+print(f"Die 2: {die2}")
+print(f"Total value :{total}")
